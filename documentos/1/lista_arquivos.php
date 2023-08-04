67068862a7a3b813f787c042f1893b0550353a7f
Yuukne <92035218+Yuukne@users.noreply.github.com>
2023-08-04 09:23:21 -0300

    1

--- documentos/1/lista_arquivos.php
+++ documentos/1/lista_arquivos.php
@@ -3,19 +3,33 @@
 $pasta = "caminho_da_pasta";
 $arquivos = scandir($pasta);
 
-$listaArquivos = [];
+echo '<!DOCTYPE html>';
+echo '<html lang="pt-br">';
+echo '<head>';
+echo '<meta charset="UTF-8">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<title>Lista de Arquivos</title>';
+echo '<style>';
+echo 'body { font-family: Arial, sans-serif; margin: 20px; }';
+echo 'h1 { color: #007BFF; }';
+echo 'ul { list-style-type: none; padding: 0; }';
+echo 'li { margin-bottom: 10px; }';
+echo 'a { color: #007BFF; text-decoration: none; }';
+echo 'a:hover { text-decoration: underline; }';
+echo '</style>';
+echo '</head>';
+echo '<body>';
+echo '<h1>Lista de Arquivos</h1>';
+echo '<ul>';
 
 foreach ($arquivos as $arquivo) {
     $extensao = pathinfo($arquivo, PATHINFO_EXTENSION);
     if ($extensao === 'pdf') {
-        $item = array(
-            'nome' => $arquivo,
-            'link' => $pasta . '/' . $arquivo
-        );
-        array_push($listaArquivos, $item);
+        echo '<li><a href="' . $pasta . '/' . $arquivo . '">' . $arquivo . '</a></li>';
     }
 }
 
-header('Content-Type: application/json');
-echo json_encode($listaArquivos);
+echo '</ul>';
+echo '</body>';
+echo '</html>';
 ?>
